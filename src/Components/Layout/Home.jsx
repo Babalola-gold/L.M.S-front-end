2f8f326e52659c538c3ef67b5a1e0be01cb4293d
Babalola Ibukun <sibukun05@gmail.com>
2024-03-12 04:20:58 -0700

    add

--- src/Components/Layout/Home.jsx
+++ src/Components/Layout/Home.jsx
@@ -1,6 +1,7 @@
-
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
+import { Carousel } from "flowbite-react";
+import img from "../../assets/grad.jpg";
 
 
 
@@ -13,15 +14,49 @@ const Home = () => {
 
   return (
     <>
-    
-      
+      <Carousel id="default-carousel" className="relative w-full" data-carousel="slide">
+        <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
+          {/* Carousel Items */}
+          <div className="hidden duration-700 ease-in-out" data-carousel-item>
+            <img src={img} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+          </div>
+          {/* Add more carousel items here */}
+        </div>
 
-      <div className='flex justify-center w-full'> 
-        <div className='flex bg-[#75DCED] ' style={{ height: '250px', lineHeight:'1.3em', borderRadius: '9px 9px 9px 9px' }}>
-                
+        {/* Carousel Navigation Buttons */}
+        <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+          <button type="button" className="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
+          {/* Add more navigation buttons for each slide */}
+        </div>
+
+        {/* Previous Button */}
+        <button type="button" className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            {/* Add previous button icon */}
+            <span className="sr-only">Previous</span>
+          </span>
+        </button>
+
+        {/* Next Button */}
+        <button type="button" className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            {/* Add next button icon */}
+            <span className="sr-only">Next</span>
+          </span>
+        </button>
+      </Carousel>
+
+
+
+
+
+
+      <div className='flex justify-center w-full'>
+        <div className='flex bg-[#75DCED] ' style={{ height: '250px', lineHeight: '1.3em', borderRadius: '9px 9px 9px 9px' }}>
+
           <svg className='w-20 ' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path d="M80 48a48 48 0 1 1 96 0A48 48 0 1 1 80 48zm64 193.7v65.1l51 51c7.1 7.1 11.8 16.2 13.4 26.1l15.2 90.9c2.9 17.4-8.9 33.9-26.3 36.8s-33.9-8.9-36.8-26.3l-14.3-85.9L66.8 320C54.8 308 48 291.7 48 274.7V186.6c0-32.4 26.2-58.6 58.6-58.6c24.1 0 46.5 12 59.9 32l47.4 71.1 10.1 5V160c0-17.7 14.3-32 32-32H384c17.7 0 32 14.3 32 32v76.2l10.1-5L473.5 160c13.3-20 35.8-32 59.9-32c32.4 0 58.6 26.2 58.6 58.6v88.1c0 17-6.7 33.3-18.7 45.3l-79.4 79.4-14.3 85.9c-2.9 17.4-19.4 29.2-36.8 26.3s-29.2-19.4-26.3-36.8l15.2-90.9c1.6-9.9 6.3-19 13.4-26.1l51-51V241.7l-19 28.5c-4.6 7-11 12.6-18.5 16.3l-59.6 29.8c-2.4 1.3-4.9 2.2-7.6 2.8c-2.6 .6-5.3 .9-7.9 .8H256.7c-2.5 .1-5-.2-7.5-.7c-2.9-.6-5.6-1.6-8.1-3l-59.5-29.8c-7.5-3.7-13.8-9.4-18.5-16.3l-19-28.5zM2.3 468.1L50.1 348.6l49.2 49.2-37.6 94c-6.6 16.4-25.2 24.4-41.6 17.8S-4.3 484.5 2.3 468.1zM512 0a48 48 0 1 1 0 96 48 48 0 1 1 0-96zm77.9 348.6l47.8 119.5c6.6 16.4-1.4 35-17.8 41.6s-35-1.4-41.6-17.8l-37.6-94 49.2-49.2z" /></svg>
           <div >
-          <br /><br />
+            <br /><br />
             <span style={{ color: '#FFFFFF', fontSize: '18px', textTransform: 'capitalize', lineHeight: '1.3em' }}>Care & Supports Of Learners</span>
             <p style={{ color: '#FFFFFF', fontSize: '15px', fontWeight: '400', lineHeight: '' }}>Fostering a culture of outstanding health , safety and learner's wellbeing in CIA is our major concern.</p>
           </div>
@@ -31,7 +66,7 @@ const Home = () => {
 
           <svg className='w-20' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M427.8 380.7l-196.5 97.8a18.6 18.6 0 0 1 -14.7 0L20.2 380.7c-4-2-4-5.3 0-7.3L67.2 350a18.7 18.7 0 0 1 14.7 0l134.8 67a18.5 18.5 0 0 0 14.7 0l134.8-67a18.6 18.6 0 0 1 14.7 0l47.1 23.4c4.1 2 4.1 5.2 0 7.2zm0-136.5l-47.1-23.4a18.6 18.6 0 0 0 -14.7 0l-134.8 67.1a18.7 18.7 0 0 1 -14.7 0L81.9 220.7a18.7 18.7 0 0 0 -14.7 0l-47.1 23.4c-4 2-4 5.3 0 7.3l196.5 97.8a18.6 18.6 0 0 0 14.7 0l196.5-97.8c4.1-2 4.1-5.3 0-7.3zM20.2 130.4l196.5 90.3a20.1 20.1 0 0 0 14.7 0l196.5-90.3c4-1.9 4-4.9 0-6.7L231.3 33.4a19.9 19.9 0 0 0 -14.7 0l-196.5 90.3c-4.1 1.9-4.1 4.9 0 6.7z" /></svg>
           <div >
-          <br /><br />
+            <br /><br />
             <span>Heritage Of Excellence</span>
             <p>Chohkmah International Academy heritage in a modern campus delivering outstanding results.</p>
           </div>
@@ -40,7 +75,7 @@ const Home = () => {
 
           <svg className='w-20' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M446.5 68c-.4-1.5-.9-3-1.4-4.5-.9-2.5-2-4.8-3.3-7.1-1.4-2.4-3-4.8-4.7-6.9-2.1-2.5-4.4-4.8-6.9-6.8-1.1-.9-2.2-1.7-3.3-2.5-1.3-.9-2.6-1.7-4-2.4-1.8-1-3.6-1.8-5.5-2.5-1.7-.7-3.5-1.3-5.4-1.7-3.8-1-7.9-1.5-12-1.5H48C21.5 32 0 53.5 0 80v352c0 4.1 .5 8.2 1.5 12 2 7.7 5.8 14.6 11 20.3 1 1.1 2.1 2.2 3.3 3.3 5.7 5.2 12.6 9 20.3 11 3.8 1 7.9 1.5 12 1.5h352c26.5 0 48-21.5 48-48V80c-.1-4.1-.6-8.2-1.6-12zM416 432c0 8.8-7.2 16-16 16H224V256H32V80c0-8.8 7.2-16 16-16h176v192h192z" /></svg>
           <div>
-          <br /><br />
+            <br /><br />
             <span >Future-Ready Curriculum</span>
             <p className='text-sm '>Our learning and teaching policy emphasizes the importance of exciting, relevant and challenging lessons to prepare learners for life.
 
@@ -48,6 +83,19 @@ const Home = () => {
           </div>
         </div>
       </div >
+
+      <div className='text-center p-20'>
+        <h2 className='text-slate-900' style={{ fontWeight: '900' }}>Welcome to Chokhmah International Academy</h2> <br /><br />
+        <div className='learner'>
+          <p style={{ margin: '0 0 20px' }}>At Chokhmah International Academy, we are committed to providing learners with a world- class educational experience. The school views every child as a self-directed learner who can fulfill his or her potential. Young minds are helped to absorb knowledge while developing courage and perspective to perceive the world boldly.</p>
+        </div>
+      </div>
+
+      <div>
+        <div><img src="" alt="" /></div>
+        <div><img src="" alt="" /></div>
+        <div><img src="" alt="" /></div>
+      </div>
     </>
   )
 }
